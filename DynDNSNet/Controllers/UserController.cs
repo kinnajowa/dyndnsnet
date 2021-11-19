2fcd7fb1ace29f16d8b9ef92f320dab4ac423f73
Jannik Taubert <uyxxq@student.kit.edu>
2021-11-19 17:44:53 +0100

    added management controller

---
+++ DynDNSNet/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynDNSNet.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        
+    }
+}
